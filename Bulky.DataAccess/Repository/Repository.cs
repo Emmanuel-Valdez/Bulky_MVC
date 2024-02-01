08516a5557ab36fe99d7c08c0b8c6d62c41fd0db
Emmanuel Valdez <evaldez.mza@gmailcom>
2024-02-01 18:05:28 -0300

    Cart UI added

--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace BulkyBook.DataAccess.Repository
 {
@@ -27,24 +28,41 @@ namespace BulkyBook.DataAccess.Repository
 			dbSet.Add(entity);
 		}
 
-		public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
+		public T Get(Expression<Func<T, bool>>? filter, string? includeProperties = null, bool tracked = false)
 		{
-			IQueryable<T> query = dbSet;
-			query = query.Where(filter);
-			if (!string.IsNullOrEmpty(includeProperties))
-			{
-				foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
-				{
-					query = query.Include(includeProp);
-				}
-			}
-			return query.FirstOrDefault();
-		}
+			IQueryable<T> query;
 
-		public IEnumerable<T> GetAll(string? includeProperties = null)
+
+            if (tracked == true)
+			{
+                query = dbSet;
+               
+            }
+			else
+			{
+                query = dbSet.AsNoTracking();
+                
+            }
+            query = query.Where(filter);
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+            return query.FirstOrDefault();
+
+        }
+
+		public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter, string? includeProperties = null)
 		{
 			IQueryable<T> query = dbSet;
-			if (!string.IsNullOrEmpty(includeProperties))
+            if(filter!= null)
+			{
+                query = query.Where(filter);
+            }
+            if (!string.IsNullOrEmpty(includeProperties))
 			{
 				foreach(var includeProp in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
 				{
